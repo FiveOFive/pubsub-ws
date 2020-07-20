c96360b9f9c9d22f1ec2576d35b5e079e03aa38e
FiveOFive <FiveOFive@users.noreply.github.com>
2020-07-19 17:55:30 -0700

    package setup

---
+++ __tests__/placeholder.ts
@@ -0,0 +1,3 @@
+test('placeholder test', () => {
+  expect(true).toBe(true);
+});
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'roots': [
+    '<rootDir>'
+  ],
+  'testMatch': [
+    '<rootDir>/__tests__/**/*.+(ts|tsx|js)'
+  ],
+  'transform': {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+}
