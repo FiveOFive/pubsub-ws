197395e685358e5029fe96fb8c7006e597fa9c99
FiveOFive <FiveOFive@users.noreply.github.com>
2020-07-20 11:43:30 -0700

    remove todo comment

--- src/webSocket.ts
+++ src/webSocket.ts
@@ -4,10 +4,6 @@ import { v4 as uuidv4 } from 'uuid';
 import WebSocket from 'ws';
 import Broker from './broker';
 
-/**
- * TODO - allow connections to make subscribe requests
- *      - have an optional AuthZ arg used to check if client is allowed to subscribe to channels
- */
 export function connect(broker: Broker) {
   return (ws: WebSocket, _request: http.IncomingMessage, channel: string): void => {
     const clientId = uuidv4();
